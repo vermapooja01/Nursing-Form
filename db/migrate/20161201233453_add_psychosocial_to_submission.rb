24c7e80ad31078bfeef0f11eb6915cfed8c4668e
Pooja Verma <pverma@miners.utep.edu>
2017-01-09 17:23:30 -0700

    modified the psychosocial and respiratory pages of form, added new fields to migration

--- db/migrate/20161201233453_add_psychosocial_to_submission.rb
+++ db/migrate/20161201233453_add_psychosocial_to_submission.rb
@@ -3,11 +3,20 @@ class AddPsychosocialToSubmission < ActiveRecord::Migration[5.0]
     add_column :submissions, :marital_status, :string
     add_column :submissions, :affect, :string
     add_column :submissions, :spiritual_needs, :string
+    add_column :submissions, :other_spiritual_needs, :string
+    add_column :submissions, :bereaving_support_given, :string
+    add_column :submissions, :hospice_care, :string
+    add_column :submissions, :substance_abuse_history, :string
+    add_column :submissions, :substance_abuse_history_type, :string
     add_column :submissions, :social_support, :string
+    add_column :submissions, :other_social_support, :string
     add_column :submissions, :advanced_directive, :string
     add_column :submissions, :resuscitation_status, :string
+    add_column :submissions, :other_resuscitation_status, :string
     add_column :submissions, :resuscitation_comments, :text
     add_column :submissions, :motion_strength, :text
+    add_column :submissions, :movement, :text
+    add_column :submissions, :gait, :text
     add_column :submissions, :grip, :text
     add_column :submissions, :icp_device, :string
     add_column :submissions, :prescribed_level, :integer
--- db/migrate/20161212224853_add_respiratory_o2therapy_to_submission.rb
+++ db/migrate/20161212224853_add_respiratory_o2therapy_to_submission.rb
@@ -3,16 +3,45 @@ class AddRespiratoryO2therapyToSubmission < ActiveRecord::Migration[5.0]
     add_column :submissions, :ventilation, :string
     add_column :submissions, :pattern, :string
     add_column :submissions, :breath_sounds, :string
+    add_column :submissions, :diminished_where, :string
+    add_column :submissions, :absent, :string
+    add_column :submissions, :absent_where, :string
+    add_column :submissions, :crackles, :string
+    add_column :submissions, :crackles_where, :string
+    add_column :submissions, :wheezes, :string
+    add_column :submissions, :wheezes_where, :string
+    add_column :submissions, :stridor, :string
+    add_column :submissions, :stridor_where, :string
+    add_column :submissions, :ronchi, :string
+    add_column :submissions, :ronchi_where, :string
     add_column :submissions, :cough, :string
     add_column :submissions, :sputum, :string
+    add_column :submissions, :respiratory_color, :string
+    add_column :submissions, :other_color, :string
     add_column :submissions, :consistency, :string
+    add_column :submissions, :other_consistency, :string
     add_column :submissions, :amount, :string
     add_column :submissions, :amount_comments, :text
     add_column :submissions, :o2_protocol_in_place, :string
+    add_column :submissions, :o2_nc, :string
+    add_column :submissions, :o2_mask, :string
     add_column :submissions, :ventilator_settings, :string
+    add_column :submissions, :mode, :string
+    add_column :submissions, :tv, :string
+    add_column :submissions, :fio2, :string
+    add_column :submissions, :rr, :string
+    add_column :submissions, :ps, :string
+    add_column :submissions, :peep, :string
+    add_column :submissions, :peak_airway_pressure, :string
+    add_column :submissions, :other_settings, :string
     add_column :submissions, :ett_placement, :integer
-    add_column :submissions, :trach, :string
+    add_column :submissions, :trach_not_applicable, :string
+    add_column :submissions, :trach_type, :string
+    add_column :submissions, :trach_size, :integer
     add_column :submissions, :chest_tube, :string
+    add_column :submissions, :location_where, :string
+    add_column :submissions, :suction_type, :string
+    add_column :submissions, :emphysema_where, :string
     add_column :submissions, :chest_tube_comments, :text
   end
 end
