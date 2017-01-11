9d9dfef1c8212fac307ffe9f7e6dc7158e020ffc
Guillermo Vargas <gavargas22@gmail.com>
2017-01-12 04:32:32 +0800

    More filling up

--- db/migrate/20161201233453_add_psychosocial_to_submission.rb
+++ db/migrate/20161201233453_add_psychosocial_to_submission.rb
@@ -1,17 +1,17 @@
 class AddPsychosocialToSubmission < ActiveRecord::Migration[5.0]
   def change
     add_column :submissions, :marital_status, :string
-    add_column :submissions, :affect, :string
+    add_column :submissions, :affect, :json
     add_column :submissions, :spiritual_needs, :string
     add_column :submissions, :other_spiritual_needs, :string
-    add_column :submissions, :bereaving_support_given, :string
+    add_column :submissions, :bereaving_support_given, :json
     add_column :submissions, :hospice_care, :string
-    add_column :submissions, :substance_abuse_history, :string
+    add_column :submissions, :substance_abuse_history, :json
     add_column :submissions, :substance_abuse_history_type, :string
     add_column :submissions, :social_support, :string
     add_column :submissions, :other_social_support, :string
     add_column :submissions, :advanced_directive, :string
-    add_column :submissions, :resuscitation_status, :string
+    add_column :submissions, :resuscitation_status, :json
     add_column :submissions, :other_resuscitation_status, :string
     add_column :submissions, :resuscitation_comments, :text
     add_column :submissions, :motion_strength, :text
