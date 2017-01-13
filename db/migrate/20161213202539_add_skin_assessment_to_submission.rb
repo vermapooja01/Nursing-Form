f9a38afc866a8d772d86968d0c01f3a89a896e9c
Pooja Verma <pverma@miners.utep.edu>
2017-01-12 17:12:02 -0700

    Modified Forms and added fields, changed tables

--- db/migrate/20161213202539_add_skin_assessment_to_submission.rb
+++ db/migrate/20161213202539_add_skin_assessment_to_submission.rb
@@ -1,12 +1,31 @@
 class AddSkinAssessmentToSubmission < ActiveRecord::Migration[5.0]
   def change
-    add_column :submissions, :wound_assessment, :string
+    # add_column :submissions, :wound_assessment, :string
     add_column :submissions, :stage, :string
     add_column :submissions, :other_wounds, :string
-    add_column :submissions, :wound_size, :string
-    add_column :submissions, :depth, :integer
+    # add_column :submissions, :wound_size, :string
+    add_column :submissions, :venous_ulcer, :string
+    add_column :submissions, :burn, :string
+    add_column :submissions, :maceration, :string
+    add_column :submissions, :excoriation, :string
+    add_column :submissions, :diabetic_ulcer, :string
+    add_column :submissions, :arterial_ulcer, :string
+    add_column :submissions, :incision, :string
+    add_column :submissions, :scar, :string
+    add_column :submissions, :skin_tears, :string
+    # add_column :submissions, :depth, :integer
+    add_column :submissions, :venous_ulcer_depth, :integer
+    add_column :submissions, :burn_depth, :integer
+    add_column :submissions, :maceration_depth, :integer
+    add_column :submissions, :excoriation_depth, :integer
+    add_column :submissions, :diabetic_ulcer_depth, :integer
+    add_column :submissions, :arterial_ulcer_depth, :integer
+    add_column :submissions, :incision_depth, :integer
+    add_column :submissions, :scar_depth, :integer
+    add_column :submissions, :skin_tears_depth, :integer
     add_column :submissions, :undermining, :string
     add_column :submissions, :drainage, :string
+    add_column :submissions, :other_drainage, :string
     add_column :submissions, :wound_amount, :string
     add_column :submissions, :color_of_wound, :string
     add_column :submissions, :periwound_surrounding_skin, :string
--- db/migrate/20161213220328_add_pain_assessment_to_submission.rb
+++ db/migrate/20161213220328_add_pain_assessment_to_submission.rb
@@ -1,6 +1,6 @@
 class AddPainAssessmentToSubmission < ActiveRecord::Migration[5.0]
   def change
-    add_column :submissions, :pain_assessment, :string
+    # add_column :submissions, :pain_assessment, :string
     add_column :submissions, :verbal, :string
     add_column :submissions, :non_verbal, :string
     add_column :submissions, :scale_used, :string
@@ -8,10 +8,10 @@ class AddPainAssessmentToSubmission < ActiveRecord::Migration[5.0]
     add_column :submissions, :additional_measures, :string
     add_column :submissions, :pain_scale, :string
     add_column :submissions, :pain_scale_rating_scale, :string
-    add_column :submissions, :flacc_scale, :string
+    # add_column :submissions, :flacc_scale, :string
     add_column :submissions, :criteria, :string
-    add_column :submissions, :score_0_flacc, :string
-    add_column :submissions, :score_1_flacc, :string
+    add_column :submissions, :flacc_score_0, :string
+    add_column :submissions, :flacc_score_1, :string
     add_column :submissions, :score_2_flacc, :string
     add_column :submissions, :total_score_flacc, :integer
     add_column :submissions, :critical_care_pain_observation_tool, :string
