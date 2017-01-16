d75ab14486c28e3a10a8c67e9503cf4a16bc3f34
Guillermo Vargas <gavargas22@gmail.com>
2017-01-17 02:36:05 +0800

    Continuing working on form

--- db/migrate/20161213191227_add_cardiovascular_to_submission.rb
+++ db/migrate/20161213191227_add_cardiovascular_to_submission.rb
@@ -1,47 +1,47 @@
 class AddCardiovascularToSubmission < ActiveRecord::Migration[5.0]
   def change
-    # add_column :submissions, :pulses, :string
-    # add_column :submissions, :radial, :string
-    # add_column :submissions, :femoral, :string
-    # add_column :submissions, :popliteal, :string
-    # add_column :submissions, :dorsalis_pedis, :string
-    # add_column :submissions, :posterior_tibialis, :string
-    # add_column :submissions, :pulses_absent, :string
-    # add_column :submissions, :pulses_weak, :string
-    # add_column :submissions, :pulses_normal, :string
-    # add_column :submissions, :pulses_full, :string
-    # add_column :submissions, :pulses_bounding, :string
-    # add_column :submissions, :pulses_dp_doppler, :string
+    add_column :submissions, :pulses_absent, :string
+    add_column :submissions, :pulses_weak, :string
+    add_column :submissions, :pulses_normal, :string
+    add_column :submissions, :pulses_full, :string
+    add_column :submissions, :pulses_bounding, :string
+    add_column :submissions, :pulses_dp_doppler, :string
+
     add_column :submissions, :radial_absent, :string
-    add_column :submissions, :femoral_absent, :string
-    add_column :submissions, :popliteal_absent, :string
-    add_column :submissions, :dorsalis_pedis_absent, :string
-    add_column :submissions, :posterior_tibialis_absent, :string
     add_column :submissions, :radial_weak, :string
-    add_column :submissions, :femoral_weak, :string
-    add_column :submissions, :popliteal_weak, :string
-    add_column :submissions, :dorsalis_pedis_weak, :string
-    add_column :submissions, :posterior_tibialis_weak, :string
     add_column :submissions, :radial_normal, :string
-    add_column :submissions, :femoral_normal, :string
-    add_column :submissions, :popliteal_normal, :string
-    add_column :submissions, :dorsalis_pedis_normal, :string
-    add_column :submissions, :posterior_tibialis_normal, :string
     add_column :submissions, :radial_full, :string
-    add_column :submissions, :femoral_full, :string
-    add_column :submissions, :popliteal_full, :string
-    add_column :submissions, :dorsalis_pedis_full, :string
-    add_column :submissions, :posterior_tibialis_full, :string
     add_column :submissions, :radial_bounding, :string
-    add_column :submissions, :femoral_bounding, :string
-    add_column :submissions, :popliteal_bounding, :string
-    add_column :submissions, :dorsalis_pedis_bounding, :string
-    add_column :submissions, :posterior_tibialis_bounding, :string
     add_column :submissions, :radial_dp_doppler, :string
+
+    add_column :submissions, :femoral_absent, :string
+    add_column :submissions, :femoral_weak, :string
+    add_column :submissions, :femoral_normal, :string
+    add_column :submissions, :femoral_full, :string
+    add_column :submissions, :femoral_bounding, :string
     add_column :submissions, :femoral_dp_doppler, :string
+
+    add_column :submissions, :popliteal_absent, :string
+    add_column :submissions, :popliteal_weak, :string
+    add_column :submissions, :popliteal_normal, :string
+    add_column :submissions, :popliteal_full, :string
+    add_column :submissions, :popliteal_bounding, :string
     add_column :submissions, :popliteal_dp_doppler, :string
+
+    add_column :submissions, :dorsalis_pedis_absent, :string
+    add_column :submissions, :dorsalis_pedis_weak, :string
+    add_column :submissions, :dorsalis_pedis_normal, :string
+    add_column :submissions, :dorsalis_pedis_full, :string
+    add_column :submissions, :dorsalis_pedis_bounding, :string
     add_column :submissions, :dorsalis_pedis_dp_doppler, :string
+
+    add_column :submissions, :posterior_tibialis_absent, :string
+    add_column :submissions, :posterior_tibialis_weak, :string
+    add_column :submissions, :posterior_tibialis_normal, :string
+    add_column :submissions, :posterior_tibialis_full, :string
+    add_column :submissions, :posterior_tibialis_bounding, :string
     add_column :submissions, :posterior_tibialis_dp_doppler, :string
+    
     add_column :submissions, :heart_sounds, :string
     add_column :submissions, :edema, :string
     add_column :submissions, :edema_where, :string
