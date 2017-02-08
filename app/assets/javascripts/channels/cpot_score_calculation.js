bc5ae87a50c92012a73bac70cc06ebecadc8a8f8
Pooja Verma <pverma@miners.utep.edu>
2017-02-08 16:08:34 -0700

    grouped fields

--- app/assets/javascripts/channels/cpot_score_calculation.js
+++ app/assets/javascripts/channels/cpot_score_calculation.js
@@ -3,20 +3,21 @@ $(document).on('turbolinks:load', function () {
 
   // Set event listeners
   $('input.calculable_checkbox').click(function(e) {
-    calculate_sum(e);
+    calculate_sum_cpot(e);
   });
 
   // Get a function that sums the values
-  function calculate_sum(object) {
+  function calculate_sum_cpot(object) {
     // Create empty var that stores a total score value
-    var total_score = parseInt($('input#submission_cpot_total_score').val());
+    var total_score_cpot = parseInt($('input#submission_cpot_total_score').val());
     // Check what operation to do based on the status of the checkbox
     if (object.target.checked) {
-      total_score = total_score + parseInt(object.target.value);
+      total_score_cpot = total_score_cpot + parseInt(object.target.value);
     } else {
       // Substract from the total value
-      total_score = total_score - parseInt(object.target.value);
+      total_score_cpot = total_score_cpot - parseInt(object.target.value);
     }
     // Render the new value
-    $('input#submission_cpot_total_score').val(total_score.toString());
+    $('input#submission_cpot_total_score').val(total_score_cpot.toString());
+    $('input#submission_cpot_total_score').val(total_score_cpot.toString());
   }
