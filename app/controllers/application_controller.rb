17369fec856f3accc982fa0ba9522a914a37f5df
Pooja Verma <pverma@miners.utep.edu>
2017-02-17 13:12:38 -0700

    small changes in fields and labels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery with: :exception
 
+  def is_admin?
+    if current_user.kind == "admin"
+      return true
+    else
+      redirect_to root_url
+    end
+  end
+
+  def signed_in?
+    return true unless logged_in?
+  end
+
+
+
 end
