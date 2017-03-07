7c69555293283990bc588c088709d83273dbb583
Pooja Verma <pverma@miners.utep.edu>
2017-03-07 13:18:12 -0700

    added peripheral iv

--- db/migrate/20170307191312_add_peripheral_iv_documentation_to_submission.rb
+++ db/migrate/20170307191312_add_peripheral_iv_documentation_to_submission.rb
@@ -5,5 +5,16 @@ class AddPeripheralIvDocumentationToSubmission < ActiveRecord::Migration[5.0]
     add_column :submissions, :number_of_attempts_for_peripheral_iv_insertion, :integer
     add_column :submissions, :catheter_gauge, :string
     add_column :submissions, :initials, :string
+    add_column :submissions, :narrative_note, :string
+    add_column :submissions, :peripheral_iv_site_assessment_date_and_time, :datetime
+    add_column :submissions, :shift, :string
+    add_column :submissions, :iv_patent, :string
+    add_column :submissions, :signs_of_infiltration, :string
+    add_column :submissions, :signs_of_infection_or_phlebitis, :string
+    add_column :submissions, :dressing_dry_or_intact, :string
+    add_column :submissions, :dressing_change_time, :time
+    add_column :submissions, :tubing_change_time, :time
+    add_column :submissions, :your_initials, :string
+    add_column :submissions, :narrative_note_for_assessment, :string
   end
 end
