6b530d114ca5762a9cafb2817c524b8090c8c4c8
Pooja Verma <pverma@miners.utep.edu>
2017-03-21 12:51:34 -0600

    added migration for vital signs documentation

---
+++ db/migrate/20170321183948_add_vital_signs_documentation_to_submission.rb
@@ -0,0 +1,20 @@
+class AddVitalSignsDocumentationToSubmission < ActiveRecord::Migration[5.0]
+  def change
+    add_column :submissions, :vital_signs_documentation_time, :datetime
+    add_column :submissions, :t_vital_signs, :string
+    add_column :submissions, :p_vital_signs, :string
+    add_column :submissions, :r_vital_signs, :string
+    add_column :submissions, :b_p_vital_signs, :string
+    add_column :submissions, :sao2_vital_signs, :string
+    add_column :submissions, :pas_pad_vital_signs, :string
+    add_column :submissions, :pcwp_vital_signs, :string
+    add_column :submissions, :cvp_vital_signs, :string
+    add_column :submissions, :gcs_vital_signs, :string
+    add_column :submissions, :icp_vital_signs, :string
+    add_column :submissions, :i_vital_signs, :string
+    add_column :submissions, :o_vital_signs, :string
+    add_column :submissions, :other_ct_ngt_drains_vital_signs, :string
+    add_column :submissions, :titrated_meds_or_meds_treatment_based_on_patients_condition_vital_signs, :string
+    add_column :submissions, :other_relevant_notes_vital_signs, :string
+  end
+end
