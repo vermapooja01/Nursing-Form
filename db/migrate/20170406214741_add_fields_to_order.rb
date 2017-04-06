cbdd27478da6c65d6914b1470fcad1368366bbcd
Pooja Verma <pverma@miners.utep.edu>
2017-04-06 17:01:08 -0600

    added order.html.haml

--- db/migrate/20170406214741_add_fields_to_order.rb
+++ db/migrate/20170406214741_add_fields_to_order.rb
@@ -1,5 +1,8 @@
 class AddFieldsToOrder < ActiveRecord::Migration[5.0]
   def change
     add_column :orders, :patient_name_order, :string
+    add_column :orders, :ordered_by, :string
+    add_column :orders, :verification_date_and_time, :datetime
+    add_column :orders, :verifier_name, :string
   end
 end
