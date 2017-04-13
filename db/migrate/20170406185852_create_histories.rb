78803d687963366f037575bc0ca02fd176652013
Pooja Verma <pverma@miners.utep.edu>
2017-04-13 12:56:46 -0600

    added labs to admin

--- db/migrate/20170406185852_create_histories.rb
+++ db/migrate/20170406185852_create_histories.rb
@@ -1,7 +1,7 @@
 class CreateHistories < ActiveRecord::Migration[5.0]
   def change
     create_table :histories do |t|
-      t.string :name
+      t.string :patient_name_history
       t.datetime :date_of_admission
       t.integer :mr_number
 
