4ed0dd77e8417386888414519f18fe09a29bbc3f
Guillermo Vargas <gavargas22@gmail.com>
2017-04-14 14:08:56 -0600

    Time is working

--- app/assets/javascripts/admin/time_zones.js
+++ app/assets/javascripts/admin/time_zones.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function () {
-  // var formattedTime = (moment($('td.date-object').html())).format('MMMM Do YYYY, h:mm:ss A');
+  var formattedTime = (moment($('td.date-object').html())).format('MMMM Do YYYY, h:mm:ss A');
   // var elPasoTime = utcTime.format('MMMM Do YYYY, h:mm:ss A');
   // Render new El Paso Time
-  // $('td.date-object').html(formattedTime);
+  $('td.date-object').html(formattedTime);
 });
