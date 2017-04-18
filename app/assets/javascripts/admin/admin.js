89b04eda534a5bb4be92cea1f074fef6fa5be7fb
Guillermo Vargas <gavargas22@gmail.com>
2017-04-18 15:14:46 -0600

    Adding more interface pieces

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -10,8 +10,24 @@
 //= require moment.js/moment-timezone-with-data-2010-2020.min
 //= require daterangepicker
 //= require bootstrap-datetimepicker
+//= require datepicker/datepicker.min
 
 //= require admin/time_zones
 //= require admin/labs
 //= require admin/patients
 //= require admin/histories
+//= require admin/mars
+
+
+$(document).on('turbolinks:load', function () {
+  // Date Time Picker
+  $('.date_and_time_picker').datetimepicker({
+    inline: true,
+    sideBySide: true
+  });
+  $('.date_and_time_picker').on('dp.change', function (e) {
+    // Conversion to momentjs data object
+    var browserDate = moment(e.date._d).format();
+    $(e.target).children().find('input[type="hidden"]').val(String(browserDate));
+  });
+});
--- app/assets/javascripts/admin/labs.js
+++ app/assets/javascripts/admin/labs.js
@@ -1,13 +1,13 @@
-$(document).on('turbolinks:load', function () {
-  $('#date_and_time_drawn_picker').datetimepicker({
-    inline: true,
-    sideBySide: true,
-    defaultDate: $('#lab_date_and_time_drawn').val()
-  });
-  $("#date_and_time_drawn_picker").on("dp.change", function (e) {
-    // Conversion to momentjs data object
-    var browserDate = moment(e.date._d).format();
-    $('#lab_date_and_time_drawn').val(String(browserDate));
-  });
-
-});
+// $(document).on('turbolinks:load', function () {
+//   $('#date_and_time_drawn_picker').datetimepicker({
+//     inline: true,
+//     sideBySide: true,
+//     defaultDate: $('#lab_date_and_time_drawn').val()
+//   });
+//   $("#date_and_time_drawn_picker").on("dp.change", function (e) {
+//     // Conversion to momentjs data object
+//     var browserDate = moment(e.date._d).format();
+//     $('#lab_date_and_time_drawn').val(String(browserDate));
+//   });
+//
+// });
