47ab8a4b20dd7688580db195178cc163f923760a
Guillermo Vargas <gavargas22@gmail.com>
2017-04-18 16:23:16 -0600

    Wow! I made Javascript that worked on the first try!

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -20,14 +20,44 @@
 
 
 $(document).on('turbolinks:load', function () {
-  // Date Time Picker
-  $('.date_and_time_picker').datetimepicker({
-    inline: true,
-    sideBySide: true
+  // Generalized Date Time Picker JS functions
+  // Get all the objects that are datetime pickers
+  var dateTimePickerObjects = $('.date_and_time_picker');
+  // Execute a loop on all the objects
+  dateTimePickerObjects.each(function(index) {
+    // Initialize on each date time picker the datetime function
+    $(this).datetimepicker({
+      inline: true,
+      sideBySide: true,
+      defaultDate: getDefaultDate(this)
+    });
   });
-  $('.date_and_time_picker').on('dp.change', function (e) {
+
+  // Set up event listeners for the objects
+  $(this).on('dp.change', function (e) {
     // Conversion to momentjs data object
     var browserDate = moment(e.date._d).format();
-    $(e.target).children().find('input[type="hidden"]').val(String(browserDate));
+    // Convert the date object into a string
+    var dateString = String(browserDate);
+    // Get the object that is in charge of setting the value for the date
+    var dateSetter = $(e.target).children('input[type="hidden"]');
+    // Set the value of the object.
+    dateSetter.val(dateString);
   });
+
+  function getDefaultDate(dateTimeObject) {
+    // Get the children of the object that has datetime
+    var defaultDateField = $(dateTimeObject).children('input[type="hidden"]');
+    // Declare empty variable that stores the date that was set through the form IF it was set
+    var defaultDate = "";
+    // Check to see if the default date is not nil
+    if (defaultDateField.val() != null) {
+      defaultDate = defaultDateField.val();
+    }
+    else {
+      // Do nothing
+    }
+    // Return the default date
+    return defaultDate;
+  }
 });
--- app/assets/javascripts/admin/time_zones.js
+++ app/assets/javascripts/admin/time_zones.js
@@ -1,6 +1,20 @@
 $(document).on('turbolinks:load', function () {
-  var formattedTime = (moment($('td.date-object').html())).format('MMMM Do YYYY, h:mm:ss A');
-  // var elPasoTime = utcTime.format('MMMM Do YYYY, h:mm:ss A');
-  // Render new El Paso Time
-  $('td.date-object').html(formattedTime);
+  // Get all the table cells that have dates
+  var cellsWithDateTimeString = $('td.date-object')
+  // Execute time conversion for all the cells that have dates and times
+  cellsWithDateTimeString.each(function(index) {
+    // Store the contents of the cell
+    var dateString = this.innerHTML;
+    // Convert the date using the function
+    var formattedDateTime = formatDate(dateString);
+    // Set the new value of the cell
+    this.innerHTML = formattedDateTime;
+  });
+  // A function that converts the date to proper format
+  function formatDate(dateString) {
+    // Use timezone.js to convert the dates and times
+    var formattedTime = (moment(dateString)).format('MMMM Do YYYY, h:mm:ss A');
+    // return the converted date time
+    return formattedTime
+  }
 });
